3b95d1e097404d666a721a412eb15df70f50a474
kuodilbert <dilbert.kuo@garmin.com>
2020-12-29 18:46:04 +0800

    IXWW22: Create a batch file to merge BT & MCU F/W
    Description:
    Merged_BT_MCU_FW.bat can merge BT image & MCU image into one binary file
    
    Testing and Verification Instructions(if required):
    1. pick this patch
    2. build MCU F/W
    3. execute Merged_BT_MCU_FW.bat in bat folder
    4. use boot_image.bin to update LinkCard via Mfg Tool
    5. check BT and MCU work normally
    
    Integration Instructions(if any):
    None
    
    Type: New Feature
    Issue: AUTOOEM-IXWW22-2780
    Change-Id: If3d5dfa43490c5d37061bb571d541bbbee70136f
    Reviewed-on: https://gerrit.garmin.com.tw/code-review/c/autooem/products/nxp/rt1172/+/79213
    Reviewed-by: Kuo, Dilbert <dilbert.kuo@garmin.com>
    Reviewed-by: Huang, Kenny <kenny.huang@garmin.com>
    Tested-by: Kuo, Dilbert <dilbert.kuo@garmin.com>
    Approve-Change: Kuo, Dilbert <dilbert.kuo@garmin.com>

---
+++ bat/Merged_BT_MCU_FW_for_Release.bat
@@ -0,0 +1,8 @@
+@echo off
+
+set /p Param=Please input the start address of BT Mini Driver + BT F/W(e.g 0x800100):
+::echo %Param%
+echo Merge BT F/W and MCU F/W within one Merged File...
+
+srec_cat.exe ..\submodule\cyw89820\Release_FW\BT_mdflash.bin -Binary -offset %Param% ..\Release\LinkCard-RT1176.bin -Binary -o boot_image.bin -Binary
+
