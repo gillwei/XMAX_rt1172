c712294e85279a51aa8b8746bf3a929eeae323b9
kuodilbert <dilbert.kuo@garmin.com>
2021-01-06 10:43:43 +0800

    IXWW22: Modify batch file for Jenkins build code
    Description:
    Modify Merged_BT_MCU_for_Release.bat for Jenkins building code
    
    Testing and Verification Instructions(if required)
    1. use Jenkins to build IXWW22
    2. download binary to IXWW22
    3. check system work well
    Jenkins link:
    https://ci.aoemsw.garmin.com/view/Embedded/view/Release-Build/job/Embedded-Man-IXWW22-SQE-release/build?delay=0sec
    
    Type: New Feature
    Issue: AUTOOEM-IXWW22-2780
    Change-Id: I8845215e695cfd629983ad25f3be7eae9f9f13d5
    Reviewed-on: https://gerrit.garmin.com.tw/code-review/c/autooem/products/nxp/rt1172/+/79548
    Reviewed-by: Kuo, Dilbert <dilbert.kuo@garmin.com>
    Tested-by: Kuo, Dilbert <dilbert.kuo@garmin.com>
    Approve-Change: Kuo, Dilbert <dilbert.kuo@garmin.com>

--- bat/Merged_BT_MCU_FW_for_Release.bat
+++ bat/Merged_BT_MCU_FW_for_Release.bat
@@ -1,8 +1,43 @@
 @echo off
 
-set /p Param=Please input the start address of BT Mini Driver + BT F/W(e.g 0x800100):
-::echo %Param%
-echo Merge BT F/W and MCU F/W within one Merged File...
+cd..
 
-srec_cat.exe ..\submodule\cyw89820\Release_FW\BT_mdflash.bin -Binary -offset %Param% ..\Release\LinkCard-RT1176.bin -Binary -o boot_image.bin -Binary
+SET CURRENT_PATH=%CD%
+
+echo %CURRENT_PATH%
+
+REM path to GNU tools and compiler: arm-none-eabi-gcc.exe, ....
+SET TOOLCHAIN_PATH=%IDE_PATH%\ide\tools\bin
+
+REM variable to the command line Eclipse IDE executable
+SET IDE=%IDE_PATH%\ide\mcuxpressoidec.exe
+
+echo Extending PATH if not already present
+echo %PATH%|findstr /i /c:"%TOOLCHAIN_PATH:"=%">nul || set PATH=%PATH%;%TOOLCHAIN_PATH%
+
+echo Copy MultiLanguage Fonts
+xcopy .\EmbeddedWizard\MultiLanguage .\EmbeddedWizard\GeneratedCode /y
+
+echo Clean and Build Project...
+
+REM -cleanbuild on a build target: this does a 'clean' only on the build target, no build
+"%IDE%" -nosplash --launcher.suppressErrors -application org.eclipse.cdt.managedbuilder.core.headlessbuild -printErrorMarkers -import %CURRENT_PATH% -cleanBuild LinkCard-RT1176/Release
+
+cd /d %TOOLCHAIN_PATH%
+
+echo Transform to Binary File...
+arm-none-eabi-objcopy -v -O binary %CURRENT_PATH%\Release\LinkCard-RT1176.axf %CURRENT_PATH%\Release\LinkCard-RT1176.bin
+
+cd /d %CURRENT_PATH%\bat
+
+set one=%1
+set two=%2
+
+echo Merge BT F/W and MCU F/W within one Merged File...
+if "%one%" NEQ "" (
+    srec_cat.exe ..\submodule\cyw89820\Release_FW\BT_mdflash.bin -Binary -offset %1 ..\Release\LinkCard-RT1176.bin -Binary -o boot_image.bin -Binary
+    if "%two%" NEQ "" (
+        srec_cat.exe boot_image.bin -Binary -E 0x800000 0x800002 -GEN 0x800000 0x800002 -CONSTant_Big_Endian %2 2 -O boot_image.bin -Binary
+	)
+)
 
