879de51f20a35f8bc185871c68e01db8c970585a
Edward Liao <Edward.Liao@garmin.com>
2021-04-13 09:53:42 +0800

    Update debug build script
    Copy from Merged_BT_MCU_FW_for_Release.bat to
    Merged_BT_MCU_FW_for_Debug.bat and replace "Release" with "Debug".
    
    Testing and Verification Instructions:
    1. Start the Windows command prompt.
    2. Enter the bat subfolder.
    3. Run "Merged_BT_MCU_FW_for_Debug.bat 0x800100 0x0104".
    => The "boot_image.bin" should be generated in the bat folder.
    
    Issue: Autooem-IXWW22-136
    Change-Id: I689bff54828c6e20271c806b0df54b174103ae85
    Type: New Feature
    Reviewed-on: https://gerrit.garmin.com.tw/code-review/c/autooem/products/nxp/rt1172/+/84327
    Reviewed-by: Kuo, Dilbert <dilbert.kuo@garmin.com>
    Reviewed-by: Liao, Edward <edward.liao@garmin.com>
    Tested-by: Kuo, Dilbert <dilbert.kuo@garmin.com>
    Tested-by: Liao, Edward <edward.liao@garmin.com>
    Approve-Change: Liao, Edward <edward.liao@garmin.com>

--- bat/Merged_BT_MCU_FW_for_Debug.bat
+++ bat/Merged_BT_MCU_FW_for_Debug.bat
@@ -1,8 +1,43 @@
 @echo off
 
-set /p Param=Please input the start address of BT Mini Driver + BT F/W(e.g 0x800100):
-::echo %Param%
+cd..
+
+SET CURRENT_PATH=%CD%
+SET BT_ADDRESS=%1
+SET BT_VERSION=%2
+SET MCU_DATA_FOLDER=%3
+
+echo %CURRENT_PATH%
+
+REM path to GNU tools and compiler: arm-none-eabi-gcc.exe, ....
+SET TOOLCHAIN_PATH=%IDE_PATH%\ide\tools\bin
+
+REM variable to the command line Eclipse IDE executable
+SET IDE=%IDE_PATH%\ide\mcuxpressoidec.exe
+
+echo Extending PATH if not already present
+echo %PATH%|findstr /i /c:"%TOOLCHAIN_PATH:"=%">nul || set PATH=%PATH%;%TOOLCHAIN_PATH%
+
+echo Clean and Build Project...
+
+REM -cleanbuild on a build target: this does a 'clean' only on the build target, no build
+if "%MCU_DATA_FOLDER%" NEQ "" (
+    "%IDE%" -nosplash --launcher.suppressErrors -application org.eclipse.cdt.managedbuilder.core.headlessbuild -printErrorMarkers -import "%CURRENT_PATH%" -cleanBuild LinkCard-RT1172/Debug -data "%MCU_DATA_FOLDER%"
+) else (
+    "%IDE%" -nosplash --launcher.suppressErrors -application org.eclipse.cdt.managedbuilder.core.headlessbuild -printErrorMarkers -import "%CURRENT_PATH%" -cleanBuild LinkCard-RT1172/Debug
+)
+
+cd /d %TOOLCHAIN_PATH%
+
+echo Transform to Binary File...
+arm-none-eabi-objcopy -v -O binary %CURRENT_PATH%\Debug\LinkCard-RT1172.axf %CURRENT_PATH%\Debug\LinkCard-RT1172.bin
+
+cd /d %CURRENT_PATH%\bat
+
 echo Merge BT F/W and MCU F/W within one Merged File...
-
-srec_cat.exe ..\submodule\cyw89820\Release_FW\BT_mdflash.bin -Binary -offset %Param% ..\Debug\LinkCard-RT1172.bin -Binary -o boot_image.bin -Binary
-
+if "%BT_ADDRESS%" NEQ "" (
+    srec_cat.exe ..\submodule\cyw89820\Release_FW\BT_mdflash.bin -Binary -offset %BT_ADDRESS% ..\Debug\LinkCard-RT1172.bin -Binary -o boot_image.bin -Binary
+    if "%BT_VERSION%" NEQ "" (
+        srec_cat.exe boot_image.bin -Binary -E 0x800000 0x800002 -GEN 0x800000 0x800002 -CONSTant_Big_Endian %BT_VERSION% 2 -O boot_image.bin -Binary
+	)
+)
